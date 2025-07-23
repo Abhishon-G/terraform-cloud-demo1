36c41e3e5978b061fc97bd0033d25d0220d83d02
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2025-07-23 17:55:21 +0530

    Update ec2-instance.tf
    last commit

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -2,11 +2,11 @@
 resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.instance_type
-  key_name      = "vmr-waf"
+  key_name      = "n8n-madhu"
   user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
-    "Name" = "Terraform-Cloud"
+    "Name" = "Terraform-Cloud-vcube137"
   }
 }
 
